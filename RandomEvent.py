6736f70e763a2eb106544e5f7ef001f49cd1e57b
iwixon <ian.wixon@gmail.com>
2014-12-07 18:40:42 -0500

    adding events to randomevents

--- RandomEvent.py
+++ RandomEvent.py
@@ -1,6 +1,30 @@
 import random
 import logging
 import shared
+import simulation
+"""
+Frogs                                 - random farm goes to Both
+Mining company Bankrupt - random mine goes Both
+
+Water to blood                    - random farm goes Baren
+Experimental fields             - random farm production rate * 3  
+
+Mine collapse                      - random Mine goes Barren
+Strike Gold                        - random Mine production rate * 3
+
+Darkness                          - Food multiplier to 0
+Long summer                    - Food multiplier to 1.5
+    
+Death of the Firstborn      - 5 people die
+Baby Boomers                  - 5 people born
+        
+Livestock Diseased          - Flat food loss (20)
+Victory Gardens               - Flat food buff (20) 
+
+Locusts                            - Food history set to 0
+"""
+
+
 
 class Event:
         def __init__(self, message, messageOver, eventLength, effect, inverseEffect):
@@ -31,9 +55,21 @@ class RandomEvent:
         self.eventList = []
         self.randomEvents = [
 
-        # ["plague on", "plague off", 1, lambda: self.addFoodFlatBuff(100), lambda: self.addFoodFlatBuff(-100)]
-        ["plague on", "plague off", 1, lambda: self.addFoodFlatBuff(0), lambda: self.addFoodFlatBuff(0)]
+        [
+        "The town has been smitten! And the frogs shall come up both on thee, and upon thy people, and upon all thy servants. - A random farm has been destroyed", 
+        None, 1, lambda: self.tileTo(shared.tileState.Farm, shared.tileType.Both), lambda: None
+        ],
 
+        [
+        "A mining company has gone bankrupt! Their mine has been abandoned.",
+        None, 1, lambda: self.tileTo(shared.tileState.Mine, shared.tileType.Both), lambda: None
+        ],
+
+        [
+        "The town has been smitten! And upon their ponds, and upon all their pools of water, that they may become blood; and that there may be blood throughout all the land! - A farm has gone barren!",
+        None, 1, lambda: self.tileTo(shared.tileState.Farm, shared.TileType.Barren)
+        ]
+        
         ]
 
     def addProductionFlatBuff(self, buff):
@@ -48,6 +84,38 @@ class RandomEvent:
     def addFoodRateBuff(self, buff):
         self.simState.statusEffects[3] += buff
 
+    def tileTo(self, tileType, destState):
+        # tileType = shared.tileState.Farm or shared.tileState.Mine
+        tileList = []
+        for pos, iterTile in self.simState.land.items(): 
+            if iterTile.state is tileType:
+                tileList.append(iterTile)
+        choice = random.choice(tileList).tile
+        choice.state = none
+        choice.type = destState
+
+    def rateChange(self, tileType, rate):
+        # tileType = shared.tileState.Farm or shared.tileState.Mine
+
+        tileList = []
+        for pos, iterTile in self.simState.land.items(): 
+            if iterTile.state is tileType:
+                tileList.append(iterTile)
+        choice = random.choice(tileList).tile 
+        choice.productionRate *= rate
+        choice.productionRate  = math.fabs(choice.productionRate)
+
+    def killPeople(self, numberToKill):
+        for x in range(numberToKill):
+            self.simState.population.remove(random.choice(self.simState.population))
+
+    def birthPeople(self, numberToBirth):
+        for x in range(numberToBirth):
+             self.simState.population.append(simulation.Person())
+
+    def resetFoodHistory(self):
+        self.simState.foodHistory = []
+
     def addEvents(self):
         for event in self.randomEvents:
             message, messageOver, eventLength, effect, inverseEffect = event
@@ -59,8 +127,7 @@ class RandomEvent:
         for x in self.eventList:
             if x in self.activeEventList: continue
             else: allowedEvents.append(x) 
-        if random.random() < .25 and len(allowedEvents) > 0: 
-        # if len(allowedEvents) >  0:
+        if random.random() < .05 and len(allowedEvents) > 0: 
             self.activeEventList.append(random.choice(allowedEvents))
             logging.debug("[Random Event - Fired]")
 
